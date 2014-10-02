21fd3f638ea800e21cc6a4fbf4977750c205fc19
Giliarle Souza <giliarle.souza@nordhoteis.com.br>
2014-10-02 09:56:15 -0300

    Commit teste 2

---
+++ verificausuario.php
@@ -0,0 +1,14 @@
+<?php
+
+$cpf = $_POST['cpf'];
+$consultasenha =" SELECT cpf_usuario from usuario";
+
+
+if (cpf == consultasenha ){
+	header("C:\Users\Giliarle.souza\Desktop\projeto_hotspot\projeto_hotspot\login_antigo.html");
+}
+
+else
+	header("C:\Users\Giliarle.souza\Desktop\projeto_hotspot\projeto_hotspot\login_antigo.html");
+
+php?>
